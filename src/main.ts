c91bf22eeca91002f8e7a4e5e84827fb607f149e
rasenshuriken <omerozoglu.1@gmail.com>
2023-03-01 14:42:03 +0300

    feat: Install todo module

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 /**
  * The bootstrap() function is the entry point to the application.
- * It is responsible for creating the Nest application instance,
- * and then calling the NestFactory.create() method to create a new Nest application instance.
- * The create() method accepts a single argument, which is the root module of the application.
- * The root module is the AppModule class, which is imported from the app.module.ts file.
- * The create() method returns a Promise that resolves to an INestApplication object.
  */
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
