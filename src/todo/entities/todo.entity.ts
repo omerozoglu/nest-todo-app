9b90c2ed18514931be07881ff0d1ac5bf854da54
rasenshuriken <omerozoglu.1@gmail.com>
2023-03-01 17:43:56 +0300

    fix: Update todo entitiy.

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,3 +1,21 @@
-export class Todo {
-    
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Todos {
+  @PrimaryGeneratedColumn()
+  uuid: string;
+  @Column({ type: 'varchar', length: 256, name: 'task_name' })
+  taskName: string;
+  @Column({ type: 'varchar', length: 256 })
+  description: string;
+  @Column({ type: 'varchar', length: 256 })
+  priority: string;
+  @Column({ type: 'date', name: 'due_date' })
+  dueDate: Date;
+  @Column({ type: 'date', name: 'complated_date' })
+  complatedDate: Date;
+  @Column({ type: 'boolean' })
+  status: boolean;
+  @Column({ type: 'varchar', length: 256 })
+  category: string;
 }
