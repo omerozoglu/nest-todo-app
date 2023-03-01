daacd2a01fdf2c4243bb9c02de95f2a412fa38f8
rasenshuriken <omerozoglu.1@gmail.com>
2023-03-01 17:44:32 +0300

    fix: Update todo controller and module.

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { Todos } from './entities/todo.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todos])],
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoService],
 })
-export class TodoModule {}
+export class TodoModule { }
