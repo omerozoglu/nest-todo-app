263c147490fe9b74021354cce69259553c157d90
rasenshuriken <omerozoglu.1@gmail.com>
2023-03-02 14:26:00 +0300

    refactor: Add decorator in CreateToDoDto

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,23 +1,33 @@
+import { IsOptional, IsUUID } from 'class-validator';
+import { IsDate } from 'class-validator/types/decorator/typechecker/IsDate';
 import { Entity, Column } from 'typeorm';
 
 @Entity()
 export class CreateTodoDto {
   @Column({ type: 'varchar', length: 256, name: 'task_name' })
   taskName: string;
+  @IsOptional()
   @Column({ type: 'varchar', length: 256 })
   description: string;
   @Column({ type: 'varchar', length: 256 })
   priority: string;
+  @IsDate()
   @Column({ type: 'date', name: 'due_date' })
   dueDate: Date;
+  @IsOptional()
+  @IsDate()
   @Column({ type: 'date', name: 'completed_date' })
   completedDate: Date;
   @Column({ type: 'boolean' })
   status: boolean;
+  @IsOptional()
   @Column({ type: 'varchar', length: 256 })
   category: string;
+  @IsOptional()
+  @IsUUID()
   @Column({ type: 'varchar', length: 256, name: 'assigned_to' })
   assignedTo: string;
+  @IsUUID()
   @Column({
     type: 'varchar',
     length: 256,
