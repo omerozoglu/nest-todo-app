6a7bb9eb1eae61d1ee9093cb71df8df181bc8498
rasenshuriken <omerozoglu.1@gmail.com>
2023-03-02 14:20:56 +0300

    refactor: Add decorator in CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,18 +1,27 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { IsOptional } from 'class-validator/types/decorator/common/IsOptional';
 import { Entity, Column } from 'typeorm';
 
 @Entity()
 export class CreateUserDto {
-    @Column({ type: 'varchar', length: 255 })
-    name: string;
+  @IsOptional()
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
 
-    @Column({ type: 'varchar', length: 255 })
-    username: string;
+  @IsNotEmpty()
+  @Column({ type: 'varchar', length: 255 })
+  username: string;
 
-    @IsEmail()
-    @Column({ type: 'varchar', length: 255 })
-    email: string;
+  @IsEmail()
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
 
-    @Column({ type: 'varchar', length: 255 })
-    password: string;
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    // minNumbers: 1,
+  })
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
 }
